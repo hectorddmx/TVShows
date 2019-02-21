b8187e0591276858f6592febaba91c059f283a9e
Héctor De Diego <lecksfrawen@gmail.com>
2019-02-21 14:53:40 -0600

    Added project and it's dependencies

---
+++ TVShows/TVShows/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TVShows
+//
+//  Created by Hector de Diego on 2/21/19.
+//  Copyright © 2019 hector.dd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
---
+++ TVShows/TVShowsTests/TVShowsTests.swift
@@ -0,0 +1,34 @@
+//
+//  TVShowsTests.swift
+//  TVShowsTests
+//
+//  Created by Hector de Diego on 2/21/19.
+//  Copyright © 2019 hector.dd. All rights reserved.
+//
+
+import XCTest
+@testable import TVShows
+
+class TVShowsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
