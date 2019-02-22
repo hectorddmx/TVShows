889f0e7912572c27d1dac4e9c8cfc9e9823922dc
Héctor De Diego <lecksfrawen@gmail.com>
2019-02-22 12:21:00 -0600

    Added TVShow Cell
    Signed-off-by: Héctor De Diego <lecksfrawen@gmail.com>

---
+++ TVShows/TVShows/UIClasses/TVShowTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TVShowTableViewCell.swift
+//  TVShows
+//
+//  Created by Hector de Diego on 2/22/19.
+//  Copyright © 2019 hector.dd. All rights reserved.
+//
+
+import UIKit
+
+class TVShowTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
