67a5e99635e30569f9e56f7b1d4cfbbf6c178181
Héctor De Diego <lecksfrawen@gmail.com>
2019-02-22 10:24:37 -0600

    Added TVShows model structs
    Signed-off-by: Héctor De Diego <lecksfrawen@gmail.com>

---
+++ TVShows/TVShows/Models/Externals.swift
@@ -0,0 +1,14 @@
+//
+//  Externals.swift
+//  TVShows
+//
+//  Created by Hector de Diego on 2/22/19.
+//  Copyright © 2019 hector.dd. All rights reserved.
+//
+
+import Foundation
+
+
+struct Externals: Codable {
+  let imdb: String
+}
